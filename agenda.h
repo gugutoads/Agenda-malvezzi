637cf34632ba575fd054916d05e83967bdc1dc69
KalebeFerreira <kalebeferreira622@gmail.com>
2024-08-17 16:18:23 -0300

    Update agenda.h
    criei uma struct para agenda e os prototipos das funções

--- agenda.h
+++ agenda.h
@@ -1,11 +1,28 @@
+#ifndef agendaESTATICA_H
+#define agendaESTATICA_H
 
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
 
+#define MAX[100]
 
-struct contato {  //criando uma struct para os contatos
+typedef struct contatos{   //criando uma struct para os contatos
+	char nome[50];
+	char email[50];
+	char numero[11];
+	
+}contatos;
 
-	char nome [50];
-	int telefone;
-	char email [50];
-};
+typedef struct{            //criando uma struct para agenda
+	Contatos contatos [MAX];
+	int total_contatos;
+}agenda;
 
+void inicializar_Agenda(Agenda *agenda);
+int adicionar_Contato(Agenda *agenda, Contato contato);
+int remover_Contato(Agenda *agenda, const char *nome);
+Contato* buscar_Contato(Agenda *agenda, const char *nome);
+void listar_Contatos(Agenda *agenda);
 
+#endif

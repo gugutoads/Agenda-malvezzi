2f53e43dda421089c1659f24d7b34787afe43373
gugutoads <gugutoad@gmail.com>
2024-08-17 13:17:21 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #define MAX 3
 #include "agenda.h"
 
@@ -7,7 +7,3 @@ struct lista {//criando a lista
 	int qntd;
 	struct contato dados [MAX];	
     };
-
-=======
-//hbjbhjbjhbjhbjhbbhj
->>>>>>> 400631e3087032a30351e7a639de782907b55713

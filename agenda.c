294f749c31b3a09e1aad4afbb97887742b9b76f7
gugutoads <gugutoad@gmail.com>
2024-08-19 14:08:56 -0300

    g

--- agenda.c
+++ agenda.c
@@ -1,2 +1,6 @@
+#include "agenda.h"
 
-
+// Função para inicializar a lista
+void inicializarLista(Lista *lista) {
+    lista->tamanho = 1; // Define o tamanho inicial como 0
+}
--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "agenda.h"
 
+// Outras operações podem ser implementadas aqui
+
+int main() {
+    Lista minhaLista;
+    inicializarLista(&minhaLista);
+
+    printf("Lista inicializada com tamanho: %d\n", minhaLista.tamanho);
+    return 0;
+}
